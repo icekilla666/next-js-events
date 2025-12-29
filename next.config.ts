94eb915008e7f5cbdc6fe802449298bdbf448c3e
icekilla666 <mmitin2005@mail.ru>
2025-12-29 23:44:12 +0300

    fix-error

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
-  }
+  },
   cacheComponents: true,
   images: {
     remotePatterns: [
